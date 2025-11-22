2c228ad06b13a5e85809a8f4d37bde0dc2ae232a
aszymaniak1710 <aszymaniak1710@gmail.com>
2025-11-22 13:17:39 +0100

    Project init

---
+++ src/main/java/pl/degree/alertly/AlertlyApplication.java
@@ -0,0 +1,13 @@
+package pl.degree.alertly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertlyApplication.class, args);
+	}
+
+}
